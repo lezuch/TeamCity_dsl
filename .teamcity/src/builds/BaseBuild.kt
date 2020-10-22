85e908cf70127c48028557c890cdb99abd673022
Mateusz <mateusz.lezuch@gmail.com>
2020-10-22 13:42:44 +0200

    id

--- .teamcity/src/builds/BaseBuild.kt
+++ .teamcity/src/builds/BaseBuild.kt
@@ -2,6 +2,8 @@ package src.builds
 
 open class BaseBuild(buildName: String)
     : PlatformBase({
+    val currentId = "${buildName}_job"
+    id(currentId)
     name = buildName
     artifactRules = """
         vsc_joba.txt
