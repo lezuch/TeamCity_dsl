32bec8daf711e4498a9aec696a0283f024bb4b28
Mateusz <mateusz.lezuch@gmail.com>
2020-10-22 12:14:27 +0200

    collect artifact

--- .teamcity/src/builds/BuildA.kt
+++ .teamcity/src/builds/BuildA.kt
@@ -1,39 +1,11 @@
 package src.builds
 
-/*object BuildA : BuildType({
-    name = "Job A"
-
+class BuildAclass()
+    : PlatformBase({
+    name = "JobA"
     artifactRules = """
         vsc_joba.txt
         target => target
     """.trimIndent()
 
-    vcs {
-        root(SpringVsc)
-    }
-
-    steps {
-        maven {
-            goals = "package"
-        }
-        script {
-            name="Colect info about VSC"
-            scriptContent="""
-                #!/bin/bash
-
-                VCS_URL=${'$'}(echo "%vcsroot.url%")
-                echo "VCS_URL = ${'$'} {VCS_URL}" > vsc_joba.txt
-            """.trimIndent()
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-})*/
-
-class BuildAclass()
-    : PlatformBase({
-    name = "BuildAclass"
 })
