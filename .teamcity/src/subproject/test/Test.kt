d16a660f086295881c348701e0f2d145fef37b83
Mateusz <mateusz.lezuch@gmail.com>
2020-10-23 14:55:26 +0200

    .

--- .teamcity/src/subproject/test/Test.kt
+++ .teamcity/src/subproject/test/Test.kt
@@ -5,6 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
 object Test : Project ({
     name = "Test"
+    buildType(GetArtifactTest)
     params{
         text("teamcity.ui.settings.readOnly", "true", display = ParameterDisplay.HIDDEN)
     }
