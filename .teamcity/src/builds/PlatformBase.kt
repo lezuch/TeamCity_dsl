ac5d70ba95606e62422f9badd4ea0bf3f0d77329
Mateusz <mateusz.lezuch@gmail.com>
2020-10-23 12:45:17 +0200

    add GetArtifact

--- .teamcity/src/builds/PlatformBase.kt
+++ .teamcity/src/builds/PlatformBase.kt
@@ -18,7 +18,7 @@ open class PlatformBase(block: BuildType.() -> Unit): BuildType({
             scriptContent="""
                 #!/bin/bash
                 
-                VCS_URL=${'$'}(echo "%vcsroot.url%")
+                VCS_URL=%vcsroot.url%"
                 echo "VCS_URL = ${'$'} {VCS_URL}" > vsc_joba.txt
             """.trimIndent()
         }
