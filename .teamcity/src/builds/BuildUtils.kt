981f39b3437a042294938ad478fd41c0573cfa5d
Mateusz <mateusz.lezuch@gmail.com>
2020-10-26 16:30:40 +0100

    add BuildUtils.kt

---
+++ .teamcity/src/builds/BuildUtils.kt
@@ -0,0 +1,29 @@
+package src.builds
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import src.vcs.SpringVsc
+
+object BuildUtils : BuildType({
+    name = "BuildUtils"
+
+    vcs {
+        root(SpringVsc, "+:utlis => .")
+    }
+    triggers {
+        vcs {
+            triggerRules = "+:test"
+            branchFilter = ""
+        }
+    }
+    steps {
+        script {
+            name = "BuildUtils"
+            scriptContent = """
+                #!/bin/bash
+                cat .test
+            """.trimIndent()
+        }
+    }
+})
