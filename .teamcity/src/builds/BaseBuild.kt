c41d4300cb4ee47b8a78367eeaccd8205000c767
Mateusz <mateusz.lezuch@gmail.com>
2020-10-26 12:56:40 +0100

    BulidPattern

--- .teamcity/src/builds/BaseBuild.kt
+++ .teamcity/src/builds/BaseBuild.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
 
 open class BaseBuild(block: BuildType.() -> Unit ) : BuildType({
-
+    buildNumberPattern = "1.0.%build.counter%"
     dependencies{
         dependency(PrepareArtifact){
             snapshot{
