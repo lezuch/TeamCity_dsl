2b2f99f5939d53d5f80d3b839c8664a2d3864900
Mateusz <mateusz.lezuch@gmail.com>
2020-10-26 14:28:41 +0100

    BulidPattern

--- .teamcity/src/builds/BaseBuild.kt
+++ .teamcity/src/builds/BaseBuild.kt
@@ -2,15 +2,18 @@ package src.builds
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
+import jetbrains.buildServer.configs.kotlin.v2019_2.ReuseBuilds
 
 open class BaseBuild(block: BuildType.() -> Unit ) : BuildType({
     buildNumberPattern = "%build.prefix%_${PrepareArtifact.depParamRefs.buildNumber}"
     dependencies{
         dependency(PrepareArtifact){
             snapshot{
+                reuseBuilds = ReuseBuilds.NO
                 onDependencyFailure =  FailureAction.CANCEL
             }
             artifacts {
+
                 cleanDestination = true
                 artifactRules = "vsc_joba.txt"
             }
