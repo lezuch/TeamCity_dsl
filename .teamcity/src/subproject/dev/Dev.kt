f42ab9e4366344ece7f74bbd507802c981377c57
Mateusz <mateusz.lezuch@gmail.com>
2020-10-26 16:31:28 +0100

    add BuildUtils to DEV

--- .teamcity/src/subproject/dev/Dev.kt
+++ .teamcity/src/subproject/dev/Dev.kt
@@ -2,10 +2,12 @@ package src.subproject.dev
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+import src.builds.BuildUtils
 
 object Dev : Project ({
     name = "Dev"
     buildType(GetArtifactDev)
+    buildType(BuildUtils)
     params{
         text("teamcity.ui.settings.readOnly", "true", display = ParameterDisplay.HIDDEN)
         text("build.prefix", "DEV", display = ParameterDisplay.HIDDEN)
