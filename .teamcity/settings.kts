49f7ffb916b3aff447c6d1f1edadea1ddb7d93a4
mlz <mateusz.lezuch@huuugegamec.com>
2020-11-20 14:11:46 +0100

    test

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,12 @@
 
 //import src.builds.BuildA
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
-import src.builds.PrepareArtifact
-import src.vcs.SpringVsc
+import src.builds.Maven
+import src.vcs.MyVcsRoot
+
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -31,7 +34,22 @@ version = "2020.1"
 
 project {
 
-    vcsRoot(SpringVsc)
-    buildType(PrepareArtifact)
+    vcsRoot(MyVcsRoot)
+    
+    val bts = sequential {
+        buildType(Maven("Build", "clean compile"))
+        parallel {
+            buildType(Maven("Fast Test", "clean test", "-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"))
+            buildType(Maven("Slow Test", "clean test", "-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"))
+        }
+        buildType(Maven("Package", "clean package", "-DskipTests"))
+    }.buildTypes()
+    
+    bts.forEach { buildType(it) }
+    bts.last().triggers {
+        vcs {
+        
+        }
+    }
 
 }
