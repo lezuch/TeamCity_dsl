7fee5d940157accde5f6694cea8a787175af8b0e
mlz <mateusz.lezuch@huuugegamec.com>
2020-12-02 09:39:47 +0100

    zmiany

--- .teamcity/src/builds/Base.kt
+++ .teamcity/src/builds/Base.kt
@@ -3,15 +3,19 @@ package src.builds
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.Template
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.toId
 import src.consts.Platform
 
 
 object Base: Template({
     name = "base"
 	steps {
+		
+		val runScriptEnvStepName = "Upload dist"
+		val runScriptEnvStepId = runScriptEnvStepName.toId()
 		script {
-			name = "Upload dist"
-			id = "upload_dist"
+			name = runScriptEnvStepName
+			id = runScriptEnvStepId
 			workingDir = "build"
 			scriptContent = """
             #!/bin/bash
