e512d56ee624771cfe0fc0e514a98b3ae16f5833
mlz <mateusz.lezuch@huuugegamec.com>
2020-12-02 10:19:15 +0100

    edit

--- .teamcity/src/env/buildDev.kt
+++ .teamcity/src/env/buildDev.kt
@@ -2,6 +2,7 @@ package src.env
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import src.builds.Base
 
 object buildDev : BuildType({
@@ -12,5 +13,12 @@ object buildDev : BuildType({
 		text("env.APPLE_PASSWORD", "PASS", display = ParameterDisplay.HIDDEN)
 	}
 	templates(Base)
-	
+	steps {
+		script {
+			name = "test"
+			scriptContent = """
+				echo "DEV"
+			""".trimIndent()
+		}
+	}
 })
