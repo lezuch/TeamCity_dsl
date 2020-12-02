4b82f5071c03344f1db8a898e77d78961fdbfe39
mlz <mateusz.lezuch@huuugegamec.com>
2020-12-02 11:13:50 +0100

    edit

--- .teamcity/src/env/DEV.kt
+++ .teamcity/src/env/DEV.kt
@@ -1,11 +1,16 @@
 package src.env
 
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
 
-object DEV : Project ({
+object DEV: Project({
 	name = "DEV"
+	params {
+		checkbox("upload_dist", "false", checked = "true", unchecked = "false", label = "Upload dist file", display = ParameterDisplay.PROMPT)
+		text("env.APPLE_USERNAME", "TEST", display = ParameterDisplay.HIDDEN)
+		text("env.APPLE_PASSWORD", "PASS", display = ParameterDisplay.HIDDEN)
+	}
 	buildType(buildDev)
-
 })
